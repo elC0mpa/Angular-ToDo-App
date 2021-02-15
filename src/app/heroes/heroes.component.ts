efbff183e0e4485c7fcbc3c0cc3eefd46d732d2c
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-02-14 22:36:13 -0300

    Importing heroes data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from './mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Compa',
-  };
+  heroes = HEROES;
   constructor() {}
 
   ngOnInit(): void {}
