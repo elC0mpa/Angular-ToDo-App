d4650cd7313c815b953e1cf40174dd6ec69ba5e4
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-03-01 00:24:31 -0300

    Entities created

---
+++ src/app/todoItem.ts
@@ -0,0 +1,8 @@
+export class TodoItem {
+  constructor(taskVal: String, completeVal: Boolean = false) {
+    this.task = taskVal;
+    this.complete = completeVal;
+  }
+  task: String;
+  complete: Boolean;
+}
