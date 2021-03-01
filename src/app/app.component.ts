ed95bc8be16c04ade0797bb1774fb4708d9e65f7
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2021-03-01 00:24:57 -0300

    Displaying todo table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todolist';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title: String = 'Tour of Heroes';
+  private list = new TodoList('Bob', [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.items.length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter((item) => !item.complete);
+  }
 }
